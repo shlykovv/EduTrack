fc0d80e5bd4828af3b37c99879589b6371d8daef
Vadim Shlykov <shlikov.vadim@gmail.com>
2025-06-07 14:18:32 +0300

    Добавлена модель User для регистрации и авторизации пользователя, добавлен hash пароля

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ login_manger.login_view = 'auth.login'
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.config')
+    app.config.from_object('config.Config')
     
     db.init_app(app)
     migrate.init_app(app, db)
