9b8d6629f72acba440ce9bdcf68f90f1afcaaa9d
Vadim Shlykov <shlikov.vadim@gmail.com>
2025-06-09 02:06:29 +0300

    Добавлена модель users_courses для связи моделей User и Course

--- app/models.py
+++ app/models.py
@@ -5,11 +5,20 @@ from app import db
 from app import login_manger
 
 
+user_courses = db.Table(
+    'user_courses',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    
+    courses = db.relationship('Course', secondary=user_courses, backref='students')
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
